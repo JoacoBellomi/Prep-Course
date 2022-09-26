adc557fd8c9c60c75c9d5cfae1cae0edec473415
JoacoBellomi <Bellomijoaquin@gmai.com>
2022-09-26 11:44:25 -0300

    First Commit

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -11,6 +11,7 @@ function mayoriaDeEdad(edad) {
   //Determinar si la persona según su edad puede ingresar a un evento.
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
+  A
 }
   
 function conection(status) {
